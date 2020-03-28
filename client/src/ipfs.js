35c94177074484a55950796d5a7b2b6be932fe95
Tony Riemer <tonyrriemer@gmail.com>
2020-03-27 23:31:58 -0400

    update config

--- client/src/ipfs.js
+++ client/src/ipfs.js
@@ -3,13 +3,12 @@ const IPFS = require('ipfs-http-client');
 // const ipfs = new IPFS({
 //     host: '127.0.0.1',
 //     port: 5001,
-//     protocol: 'http'
+//     protocol: 'https'
 // });
 
 const ipfs = new IPFS({
-    host: 'ec2-184-72-99-4.compute-1.amazonaws.com',
-    port: 5001,
-    protocol: 'http'
+    host: '127.0.0.1',
+    protocol: 'https'
 });
 
 export default ipfs;
