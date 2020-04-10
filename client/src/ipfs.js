894a4c07a8e8157a543c8d7771954b30cf5832b3
Tony Riemer <tonyrriemer@gmail.com>
2020-04-09 21:40:12 -0400

    update ipfs config, add jpgs for readme

--- client/src/ipfs.js
+++ client/src/ipfs.js
@@ -1,16 +1,16 @@
 const IPFS = require('ipfs-http-client');
 
-const ipfs = new IPFS({
-    host: '127.0.0.1',
-    port: 5002,
-    protocol: 'https'
-});
-
 // const ipfs = new IPFS({
-//     host: '3.224.116.20',
+//     host: '127.0.0.1',
 //     port: 5002,
 //     protocol: 'https'
 // });
+
+const ipfs = new IPFS({
+    host: '3.224.116.20',
+    port: 5002,
+    protocol: 'https'
+});
 
 export default ipfs;
 
